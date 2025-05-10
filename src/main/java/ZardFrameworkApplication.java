ddcff2c35c9ff4034766fd4ea2a4737ff9f3c3b6
Zard delas <aparecidohallyson@gmail.com>
2025-05-10 17:44:55 -0300

    comments

--- src/main/java/ZardFrameworkApplication.java
+++ src/main/java/ZardFrameworkApplication.java
@@ -4,19 +4,19 @@ import configurations.routes.RouterRegister;
 import configurations.Server;
 import configurations.scanners.ZardContext;
 
-
 public class ZardFrameworkApplication {
 	public static void main(String[] args) throws Exception {
 		Server app = new Server(8080);
+		//coloque aqui, o pacote principal onde se encontra suas entidades
 		EntityManager.generateSchema("entities");
-
 		ZardContext context = new ZardContext();
+		//coloque aqui, o pacote principal do seu projeto
 		context.initialize("project");
 
 		for (Object controller : context.getControllers()) {
 			RouterRegister.registerRoutes(app, controller);
 		}
-
+		// De resto, não tem ncessidade alterar.
 		app.start();
 	}
 }
--- src/main/java/configurations/scanners/ZardContext.java
+++ src/main/java/configurations/scanners/ZardContext.java
@@ -10,41 +10,63 @@ import java.lang.reflect.ParameterizedType;
 import java.util.*;
 public class ZardContext {
 
+    // Container principal de instâncias gerenciadas pelo framework (como um mini Spring)
     private final Map<Class<?>, Object> container = new HashMap<>();
 
+    // Metodo principal que inicializa todo o contexto, escaneando o pacote e instanciando os componentes
     public void initialize(String basePackage) throws Exception {
-        // Repositórios
+
+        // Instancia todos os repositórios anotados com @Repository
         for (Class<?> clazz : ClassScanner.getAnnotatedClasses(basePackage, Repository.class)) {
+            // Recupera a interface genérica implementada pelo repositório (ex: UserRepository extends GenericRepository<Users, Long>)
             ParameterizedType genericInterface = (ParameterizedType) clazz.getGenericInterfaces()[0];
+
+            // Extrai o tipo da entidade (Users, Product, etc.)
             Class<?> entityClass = (Class<?>) genericInterface.getActualTypeArguments()[0];
+
+            // Cria o repositório dinamicamente via fábrica
             Object instance = RepositoryFactory.createRepository(clazz, entityClass);
+
+            // Adiciona o repositório instanciado ao contêiner
             container.put(clazz, instance);
         }
 
-        // Serviços
+        // Instancia todos os serviços anotados com @Service
         for (Class<?> clazz : ClassScanner.getAnnotatedClasses(basePackage, Service.class)) {
+            // Cria instância do serviço e injeta as dependências necessárias
             instantiateWithDependencies(clazz);
         }
 
-        // Controladores
+        // 🔹 3. Instancia todos os controladores anotados com @RestController
         for (Class<?> clazz : ClassScanner.getAnnotatedClasses(basePackage, RestController.class)) {
+            // Cria instância do controller e injeta as dependências necessárias
             instantiateWithDependencies(clazz);
         }
     }
 
+    // Método auxiliar para criar instâncias de classes com dependências injetadas via construtor
     private void instantiateWithDependencies(Class<?> clazz) throws Exception {
+        // Pega o primeiro construtor público da classe
         Constructor<?> constructor = clazz.getConstructors()[0];
+
+        // Para cada parâmetro do construtor, busca a instância correspondente no contêiner
         Object[] params = Arrays.stream(constructor.getParameterTypes())
-                .map(container::get)
+                .map(container::get) // injeta dependência já criada
                 .toArray();
+
+        // Cria a instância da classe passando as dependências
         Object instance = constructor.newInstance(params);
+
+        // Armazena a nova instância no contêiner
         container.put(clazz, instance);
     }
 
+    // Recupera uma instância já gerenciada no contêiner
     public <T> T get(Class<T> clazz) {
         return clazz.cast(container.get(clazz));
     }
 
+    // Retorna uma lista de todas as instâncias de controllers registrados
     public Collection<Object> getControllers() {
         return container.entrySet().stream()
                 .filter(e -> e.getKey().isAnnotationPresent(RestController.class))
@@ -52,4 +74,3 @@ public class ZardContext {
                 .toList();
     }
 }
-
